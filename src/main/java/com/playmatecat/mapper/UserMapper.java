faf4b2fab1f77f068b4c9a9a1192f6e0d7c29533
Isa <Isa@Isa-PC>
2015-04-20 00:11:58 +0800

    1

---
+++ src/main/java/com/playmatecat/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.playmatecat.mapper;
+
+import java.util.Map;
+
+import com.playmatecat.modules.login.dto.UserDTO;
+
+public interface UserMapper {
+	/**
+	 * 添加一条用户记录
+	 * @param params
+	 * @return
+	 */
+	public int addUser(Map<String,Object> params);
+	
+	public UserDTO getUser();
+}
---
+++ src/main/java/com/playmatecat/test/TestMapper.java
@@ -0,0 +1,34 @@
+package com.playmatecat.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.playmatecat.mapper.UserMapper;
+import com.playmatecat.modules.login.dto.UserDTO;
+import com.playmatecat.utils.json.UtilsJson;
+
+@RunWith(SpringJUnit4ClassRunner.class)  
+@ContextConfiguration({"classpath*:/config/spring/application*.xml"})
+public class TestMapper {
+	
+	@Autowired
+	private UserMapper userMapper;
+	
+	@Test
+	public void test() {
+		UserDTO userDTO = new UserDTO();
+		userDTO.setEmail("ccc");
+		userDTO.setPassword("ccc");
+		userDTO.setLoginedAt(new java.util.Date());
+		userDTO.setNickName("ccc");
+
+		//userMapper.addUser(UtilsJson.parseObj2Map(userDTO));
+		
+		UserDTO dto2 = userMapper.getUser();
+		
+		int a = 1;
+	}
+}
